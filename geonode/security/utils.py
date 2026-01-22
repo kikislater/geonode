bb69527f87af4557ae65b1e3cfc7b7d9cd2ce713
kikislater <kikiesttoujoursla@gmail.com>
2026-01-22 11:09:59 +0400

    4.4.4

--- geonode/security/utils.py
+++ geonode/security/utils.py
@@ -106,6 +106,31 @@ def get_visible_resources(
             if not user or not user.is_authenticated or user.is_anonymous:
                 queryset = queryset.exclude(is_published=False)
 
+########################################################################################################################
+########################################################################################################################
+        # Hide resources that are published but not approved
+        # queryset = queryset.exclude(is_published=True, is_approved=False)
+        ######################################
+        ### But let user see his own datasets
+        # if user and user.is_authenticated:
+        #     queryset = queryset.exclude(
+        #         Q(is_published=True, is_approved=False) & ~Q(owner=user)
+        #         )
+        # else:
+        #     queryset = queryset.exclude(is_published=True, is_approved=False)
+        ######################################
+        # And also not published, not approved
+        if user and user.is_authenticated:
+            queryset = queryset.exclude(
+                (Q(is_published=True, is_approved=False) | Q(is_published=False, is_approved=False)) & ~Q(owner=user)
+                )
+        else:
+            queryset = queryset.exclude(
+                Q(is_published=True, is_approved=False) | Q(is_published=False, is_approved=False)
+                )
+########################################################################################################################
+########################################################################################################################
+
         # Hide Resources Belonging to Private Groups
         if private_groups_not_visibile:
             private_groups = GroupProfile.objects.filter(access="private").values("group")
@@ -580,7 +605,7 @@ class AdvancedSecurityWorkflowManager:
                     ):
                         for _perm in VIEW_PERMISSIONS + DOWNLOAD_PERMISSIONS:
                             safe_remove(prev_perms, _perm)
-            _perm_spec["groups"][ResourceGroupsAndMembersSet.anonymous_group] = list(set(prev_perms))
+#            _perm_spec["groups"][ResourceGroupsAndMembersSet.anonymous_group] = list(set(prev_perms))
 
             if ResourceGroupsAndMembersSet.registered_members_group and getattr(
                 groups_settings, "AUTO_ASSIGN_REGISTERED_MEMBERS_TO_REGISTERED_MEMBERS_GROUP_NAME", False
